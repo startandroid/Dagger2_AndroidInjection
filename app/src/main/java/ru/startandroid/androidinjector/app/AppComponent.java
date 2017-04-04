58e4e8e7c2352e4a67fe4c1ff0afdd597240a5c9
startandroid <startofandroid@gmail.com>
2017-04-05 00:51:20 +0300

    add AndroidInjectionModule

--- app/src/main/java/ru/startandroid/androidinjector/app/AppComponent.java
+++ app/src/main/java/ru/startandroid/androidinjector/app/AppComponent.java
@@ -1,8 +1,9 @@
 package ru.startandroid.androidinjector.app;
 
 import dagger.Component;
+import dagger.android.AndroidInjectionModule;
 
-@Component(modules = {AppModule.class, AppScBuildersModule.class})
+@Component(modules = {AppModule.class, AppScBuildersModule.class, AndroidInjectionModule.class})
 public interface AppComponent {
     void injectApp(App app);
 }
